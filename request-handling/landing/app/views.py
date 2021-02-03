1ea2f4997f0b9f9f5e0a95940a5d65f936fab3c2
Natalya Chekhonina <pfzw90@gmail.com>
2021-02-03 17:08:58 +0300

    done

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -12,6 +12,7 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    counter_click.update({request.GET.get('from-landing')})
     return render(request, 'index.html')
 
 
@@ -20,13 +21,28 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
+    ab_test_arg = request.GET.get('ab-test-arg')
+    counter_show.update({ab_test_arg})
+    if ab_test_arg == 'test':
+        return render(request, 'landing_alternate.html')
     return render(request, 'landing.html')
 
 
 def stats(request):
     # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
     # Для вывода результат передайте в следующем формате:
+
+    if counter_show['test'] > 0:
+        test_conversion = round(counter_click['test']/counter_show['test'], 1)
+    else:
+        test_conversion = 0
+
+    if counter_show['original'] > 0:
+        original_conversion = round(counter_click['original']/counter_show['original'], 1)
+    else:
+        original_conversion = 0
+
     return render(request, 'stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': test_conversion,
+        'original_conversion': original_conversion,
     })
